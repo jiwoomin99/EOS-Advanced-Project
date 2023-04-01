17e6dcec19e3dbca0b78fa8941a43a29a2b33048
jiwoomin99 <jiwoomin991@gmail.com>
2023-04-01 16:44:35 +0900

    week4

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:eos_chatting/screens/main_screen.dart';
+import 'package:eos_chatting/chatting/chat/chat_bubble.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(
+       MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'EOS Chatting App',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        // home: LoginSignUpScreen(),)
+         home: ChatScreen(),)
+  );
 }
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'EOS Chatting App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: LoginSignUpScreen()
-    );
-  }
-}
-
